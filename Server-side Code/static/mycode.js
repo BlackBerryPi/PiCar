d1dd17a01a44ea27c193f15857a452564b7cc1a8
BlackBerryPi <1349195047@qq.com>
2017-08-29 14:53:47 +0800

    Update mycode.js

--- Server-side Code/static/mycode.js
+++ Server-side Code/static/mycode.js
@@ -2,10 +2,23 @@ jQuery(document).ready(function(){
     jQuery("img").bind("click", function(){
         jQuery.post({
             url: "/",
-            data: {dir: jQuery(this).attr("dir")},
+            data: {
+				dir: jQuery(this).attr("dir"),
+				angle: $("#angle").val()
+			},
+            success: function(result) {
+            }
+        });
+    });
+	jQuery("#angle").bind("change", function(){
+        jQuery.post({
+            url: "/",
+            data: {
+				dir: 6,
+				angle: $("#angle").val()
+			},
             success: function(result) {
             }
         });
     });
  });
-
